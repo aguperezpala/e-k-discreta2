bc8db7034708d01a419fdf411846042247d76b04
aguperezpala <aguperezpala@d3f76956-4586-11de-b25d-9f9778916a3c>
2009-05-20 21:44:29 +0000

    ...

---
+++ trunk/edge_list.h
@@ -0,0 +1,78 @@
+/*! vamos a hacer directamente el tipo de lista arista para agilizar las cosas */
+
+/*! Practicamente lo que es esta lista consta de 4 punteros y un entero (tamaño)
+ * El primer puntero señala el comienzo de la lista, el 2 señala el final de los
+ * "NO", el 3 el final de los SI  y el 4 el elemento "actual".
+ * Tanto en el momento de la creacion como cuando se agregan elementos, estos son
+ * puestos en la cola de "SI".
+*/
+#ifndef EDGE_LIST_H
+#define EDGE_LIST_H
+
+#include <stdlib.h>
+#include "node.h"	/* edge */
+
+/* definimos el tipo */
+typedef struct s_edgeList edgeList_t;
+
+
+/*!<<debemos pasar todas las funciones a inline o hacer visible la estructura y
+ * usamos #defines para definir las funciones (Algunas)
+ */
+
+/* Funcion que crea una lista (en caso de que usemos dinamica) 
+ * constructor
+ */
+edgeList_t * el_create (void);
+
+/* destructor 
+	REQUIRES:
+		el != NULL
+*/
+void el_destroy (edgeList_t * el);
+
+
+/* Funcion que obtiene el edge acutal al que actualmente la lista 
+	REQUIRES:
+		el != NULL
+	RETURNS:
+		NULL 	si no hay elemento
+		edge 	cc
+*/
+edge_t * el_get_actual (edgeList_t * el);
+
+/* Funcion que agrega un elemento al edge_list. Vamos a usar estructuras fijas,
+ * NO dinamicas (por eficiencia...) 
+	REQUIRES:
+		el	!= NULL
+		n	!= NULL
+*/
+void el_add_edge (edgeList_t * el, unsigned int flow, unsigned int capacity, node_t * n);
+/*NOTE:la misma que antes solo que inicializa el flow en 0 */
+void el_add_edge_no_flow (edgeList_t * el,  unsigned int capacity, node_t * n);
+
+
+/* Funcion que devuelve el tamaño de la lista, osea delta
+	REQUIRES:
+		el != NULL
+*/
+unsigned int el_get_size (edgeList_t * el);
+
+
+/* Funcion que manda el edge actual de la parte SI a la parte NO
+	REQUIRES:
+		el != NULL
+*/
+void el_swap_to_si_edge (edgeList_t * el);
+
+
+/* Funcion que manda el edge actual de la parte NO a la parte SI
+	REQUIRES:
+		el != NULL
+*/
+void el_swap_to_no_edge (edgeList_t * el);
+
+
+
+
+#endif
---
+++ trunk/node.h
@@ -0,0 +1,20 @@
+#ifndef NODE_H
+#define NODE_H
+#include <stdio.h>
+#include <stdlib.h>
+/*! #include "listType.h": deberiamos nosotros (lo vamos a hacer) implementar este
+ * TAD. Eficientemente y usando esos truquitos para poder diferenciar los 2 sub
+ * conjuntos (los habilitados y deshabilitados).
+ */
+
+
+/* definimos los tipos (NO es TAN mala practica definir los 2 tipos aca mismo) */
+typedef struct s_node node_t;
+typedef struct s_edge edge_t;
+
+
+/*! los que le decia de las funciones inline, aca les paso un link
+ * http://www.greenend.org.uk/rjk/2003/03/inline.html 
+ */
+
+#endif
