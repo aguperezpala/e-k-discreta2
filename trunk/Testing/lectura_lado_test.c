a807367140ba9cfb6684a67c9d2a6ee9e82baa21
Hobborg <Hobborg@d3f76956-4586-11de-b25d-9f9778916a3c>
2009-05-26 04:39:24 +0000

    Aver

---
+++ trunk/Testing/lectura_lado_test.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main (void)
+{
+	char edge[12];
+	char vert1, vert2;
+	char *scan;
+	unsigned int cap;
+	
+
+	while (true) {
+		memset (edge, '\0', 12);
+		printf ("Enter next edge: ");
+		scanf ("%[^\n]", &edge);
+		getchar ();
+		
+		if (edge[2] != ' ') {
+			fprintf (stderr, "Finalizó la lectura de lados\n");
+			goto end;
+		}
+			
+		vert1 = edge[0];
+		vert2 = edge[1];
+		cap = (unsigned int) strtol (edge+3, &scan, 10);
+		
+		if (*scan != '\0') {
+			fprintf (stderr, "Finalizó la lectura de lados\n");
+			goto end;
+		}
+			
+		printf ("\nvert1: %c\nvert2: %c\ncapacity: %u\n",
+			vert1, vert2, cap);
+	}
+	
+	end:
+	{
+			printf ("Goodbye, thanks for beeing so gay!\n");
+			return 0;
+	}
+}
+
