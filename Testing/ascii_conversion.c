fcca1be080a20a3d6aabe4c9bf64fb1142c7bd98
Hobborg <Hobborg@d3f76956-4586-11de-b25d-9f9778916a3c>
2009-05-26 15:26:35 +0000

    Casi convertido

--- Testing/ascii_conversion.c
+++ Testing/ascii_conversion.c
@@ -3,23 +3,39 @@
 #include <stdbool.h>
 #include <ctype.h>
 
-#define is_int(x)	( ((x == '0') || (x == '1') || (x == '2') || (x == '3') \
-			   (x == '4') || (x == '5') || (x == '6') || (x == '7') \
-			   (x == '8') || (x == '9')) ? true : false )
+#define is_int(x)	( (((x) == '0') || ((x) == '1') || ((x) == '2') || ((x) == '3') || \
+			   ((x) == '4') || ((x) == '5') || ((x) == '6') || ((x) == '7') || \
+			   ((x) == '8') || ((x) == '9')) ? true : false )
 
 
-static inline bool IsInt (const char *cadena, unsigned int *entero)
+/* Devuelve el resultado de x elevado a la n-ésima potencia 
+   PRE: n > 0
+*/
+static unsigned int pow (unsigned int x, unsigned int n)
 {
-	unsigned short i, n;
+	unsigned int i, y=x;
+	
+	for (i=1 ; i<n ; i++)
+		y *= x;
+	
+	return y;
+}
+
+
+static bool IsInt (const char *cadena, unsigned int *entero)
+{
+	unsigned int i, n;
 	char c;
 	
 	n = strlen (cadena);
 	
 	for (i=0 ; i<n ; i++) {
 		c = cadena[i];
-		if (is_int (c))
-			*entero += pow (10, (n-i)) * (unsigned int) c;
-		else
+		if (is_int (c)) {
+			/* ASCII DE '0' == 48 */
+			*entero += pow (10, (n-i)) * ((unsigned int) c - 48);
+			printf ("char: %c ; int: %u\n", c, (unsigned int) c - 48);
+		} else
 			goto end2;
 	}
 	
@@ -30,27 +46,14 @@ static inline bool IsInt (const char *cadena, unsigned int *entero)
 }
 
 
-/* Devuelve el resultado de x elevado a la n-ésima potencia 
-   PRE: n > 0
-*/
-static inline unsigned int pow (unsigned int x, unsigned int n)
-{
-	unsigned int i, y=x;
-	
-	for (i=1 ; i<n ; i++)
-		y *= x;
-	
-	return y;
-}
 
-		
 int main (void)
 {
-	char c;
-	char entero[10], nonetero[10];
+	char entero[10], noentero[10];
 	unsigned int  ascii;
+/*	char c;
 	
-/*	for (c = 32; c < 127; c++) {
+	for (c = 32; c < 127; c++) {
 		printf("Ascii %d is %c\n", c, c);
 		ascii = (unsigned int) c;
 		printf("Again, %c is ascii %u\n", c, ascii);
@@ -70,6 +73,7 @@ int main (void)
 	noentero[0] = ' ';
 	noentero[0] = '0';
 	noentero[0] = '0';
+	ascii = 0;
 	if (IsInt (entero, &ascii))
 		printf ("cadena: %s\tentero: %u\n", entero, ascii);
 	
