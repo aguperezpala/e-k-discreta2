2fc3aab1ba72c07197818b1ce3f3a4512201eb50
Hobborg <Hobborg@d3f76956-4586-11de-b25d-9f9778916a3c>
2009-05-28 21:01:16 +0000

    Por hacer la resolución de listas conflictivas

---
+++ coloreo_inicial.h
@@ -0,0 +1,33 @@
+/* MACK-EK versión 1.0
+ * Implementación eficiente del algoritmo Edmonds-Karp para hallar un flujo
+ * maximal en un network
+ *
+ * Módulo de resolución de aristas conflictivas de un network.
+ * Dada una instancia de EstadoNetwork obtiene un coloreo propio para la misma
+ *
+ * Autores:  Budde, Carlos E.
+ *	     Kondrasky, Alejandro
+ *	     Pérez Paladini, Agustín
+ *	     Soldevilla, Mallku R.
+ *
+ * Revisión: Penazzi, Daniel
+ *
+ */
+ 
+#include "network.h"
+
+
+#ifndef C_INICIAL_H
+#define C_INICIAL_H
+
+
+/* Estudia la lista de aristas conflictivas de (estado) para ir resolviendo
+ * los conflictos de color uno por uno
+ *
+ * PRE: estado != NULL
+ * POS: "estado posee coloreo propio"
+ */
+int ResolverConflictos (EstadoNetwork *estado);
+
+
+#endif
