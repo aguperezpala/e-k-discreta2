3cb61ce08dde900d9e8f04db4943e190f6bc06eb
Hobborg <Hobborg@d3f76956-4586-11de-b25d-9f9778916a3c>
2009-05-28 21:01:16 +0000

    Por hacer la resolución de listas conflictivas

---
+++ trunk/coloreo_inicial.c
@@ -0,0 +1,15 @@
+#include "edge_list.h"
+#include "node.h"
+#include "coloreo_inicial.h"
+
+/* Estudia la lista de aristas conflictivas de (estado) para ir resolviendo
+ * los conflictos de color uno por uno
+ *
+ * PRE: estado != NULL
+ * POS: "estado posee coloreo propio"
+ */
+int ResolverConflictos (EstadoNetwork *estado)
+{
+	/** TODO */
+}
+
