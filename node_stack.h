7d949a3d523b15bf1d2d25d447e496afc5e72ce2
Drasky.Vanderhoff <Drasky.Vanderhoff@d3f76956-4586-11de-b25d-9f9778916a3c>
2009-06-01 13:44:05 +0000

    Mas modificaciones de a node_stack , luego se pasaran a print_stack.

--- node_stack.h
+++ node_stack.h
@@ -17,6 +17,8 @@
 #ifnedf NODE_STK_H
 #define NODE_STK_H
 
+#include "node.h"
+
 typedef struct _node_s node_s;
 
 /* Genera una nueva pila de nodos vacía
@@ -24,16 +26,7 @@ typedef struct _node_s node_s;
  *    ns = ns_create()
  * POS: ns != NULL
  */
-node_s *ns_create (void);
-
-
-/* Destruye la pila de nodos
- *
- * PRE: ns != NULL
- *    ns_destroy(ns)
- * POS: los recursos de memoria de ns han sido liberados
- */
-void ns_destroy (node_s *ns);
+node_s *ns_create (int modoinput);
 
 /* Añade un nuevo vértice 'v' al comienzo de la pila de nodos.
  *
@@ -46,11 +39,11 @@ node_s *ns_add_node (node_s *ns, u32 v);
 /* Imprime la pila en orden LIFO, es decir, el último elemento agregado
  * será el primero en ser impreso
  *
- * PRE: ps != NULL  &&  modoinput == 1,2
- *    ps_print (ps)
+ * PRE: ns != NULL  &&  modoinput == 1,2 && nodes != NULL && {ns} == {nodes}
+ *    ns_print (ns)
  * POS: modoinput == 1  =>  se considera el código ascii de 'v'
  *	modoinput == 2  =>  se considera el valor numérico de 'v'
  */
-void ps_print (print_s *ps, int modoinput);
+void ns_print (node_s *ns, int modoinput , node_t * nodes);
 
 #endif
