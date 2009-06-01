f9539a4af174c85647aebe5aec026f9f9e34012b
aguperezpala <aguperezpala@d3f76956-4586-11de-b25d-9f9778916a3c>
2009-06-01 14:23:50 +0000

    ...

--- parser_args.h
+++ parser_args.h
@@ -40,7 +40,69 @@ void pa_destroy (parserArgs_t * pa);
 int pa_parse (parserArgs_t * pa, int argc, char ** args);
 
 
+/*! ~~~~~~~~	Funciones para obtener los flags ~~~~~~~*/
+/* isNumeric 
+	REQUIRES:
+		pa != NULL
+*/
+bool pa_is_numeric (parserArgs_t * pa);
 
+
+/* calculamos el flujo?
+	REQUIRES:
+		pa != NULL
+*/
+bool pa_work_flow (parserArgs_t * pa);
+
+/* calculamos el coloreo?
+	REQUIRES:
+		pa != NULL
+*/
+bool pa_work_colour (parserArgs_t * pa);
+
+/* verbose
+	REQUIRES:
+		pa != NULL
+*/
+bool pa_verbose (parserArgs_t * pa);
+
+/* tenemos que leer incrementalmente?
+	REQUIRES:
+		pa != NULL
+	RETURNS:
+		0 en caso de que no
+		> 0 caso de que si (indicando cada cuantas lineas tenemos que leer)
+*/
+int pa_incremental (parserArgs_t * pa);
+
+
+/* tenemos que correr parcialmente el algoritmo??
+	REQUIRES:
+		pa != NULL
+	RETURNS:
+		0 en caso de que no
+		> 0 caso de que si (indicando cuantas corridas tenemos que hacer)
+*/
+int pa_partial (parserArgs_t * pa);
+
+
+/* tenemos que repetir el algoritmo de flujo para medir el tiempo
+	REQUIRES:
+		pa != NULL
+	RETURNS:
+		0 en caso de que no
+		> 0 caso de que si (indicando cuantas veces)
+*/
+int pa_max_flow_repeat (parserArgs_t * pa);
+
+/* tenemos que repetir el algoritmo de coloreo para medir el tiempo
+	REQUIRES:
+		pa != NULL
+	RETURNS:
+		0 en caso de que no
+		> 0 caso de que si (indicando cuantas corridas tenemos que hacer)
+*/
+int pa_colour_repeat (parserArgs_t * pa);
 
 
 
