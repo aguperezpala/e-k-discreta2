7da7141a55bf653f65fe345daa8220150253d629
Drasky.Vanderhoff <Drasky.Vanderhoff@d3f76956-4586-11de-b25d-9f9778916a3c>
2009-06-04 03:26:54 +0000

    Salio perfecto el unit testing de node_stack

---
+++ unit_testing/run_tests.c
@@ -0,0 +1,18 @@
+#include <check.h>
+#include <stdlib.h> /* Para tener EXIT_XXX */
+#include "test_node_stack.h"
+
+int main (void)
+{
+	int number_failed;
+	SRunner *sr = srunner_create(NULL);
+	srunner_add_suite(sr, node_stack_suite());
+
+
+	srunner_set_log(sr, "test.log");
+	srunner_run_all(sr, CK_NORMAL);
+	number_failed = srunner_ntests_failed(sr);
+	srunner_free(sr);
+	return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
