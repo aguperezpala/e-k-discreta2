5d456668ceefa0b05d50bd5d9d38001286a098a0
Drasky.Vanderhoff <Drasky.Vanderhoff@d3f76956-4586-11de-b25d-9f9778916a3c>
2009-06-05 03:26:26 +0000

    Unit testing de edgelist terminado

--- unit_testing/run_tests.c
+++ unit_testing/run_tests.c
@@ -1,13 +1,14 @@
 #include <check.h>
 #include <stdlib.h> /* Para tener EXIT_XXX */
 #include "test_node_stack.h"
+#include "test_edge_list.h"
 
 int main (void)
 {
 	int number_failed;
 	SRunner *sr = srunner_create(NULL);
 	srunner_add_suite(sr, node_stack_suite());
-
+	srunner_add_suite(sr, edge_list_suite());
 
 	srunner_set_log(sr, "test.log");
 	srunner_run_all(sr, CK_NORMAL);
---
+++ unit_testing/test_edge_list.c
@@ -0,0 +1,159 @@
+#include "test_edge_list.h"
+#include "../edge_list.h"
+#include "../edge.h"
+#include "../consts.h"
+#include <signal.h>
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Configuracion del testing*/
+#define CANT_EDGES 20
+
+/* Variables locales */
+static edge_t * e = NULL;
+static edgeList_t * el_din = NULL;
+static u32 i = 0;
+
+/* Funciones para seteo de entorno */
+
+START_TEST ( test_create_destroy_dinamic_empty_el )
+{
+	el_din = el_create ();
+	assert (el_din != NULL);
+	el_dinamic_destroy(el_din);
+	el_din= NULL;
+}
+END_TEST
+
+
+START_TEST (test_add_edge_dinamic_el)
+{
+	el_din = el_create ();
+	assert (el_din != NULL);
+
+	for(i=0; i < CANT_EDGES ; i++){
+		e = edge_create(i , i , i+1);
+		el_add_edge(el_din, e);
+	}
+}
+END_TEST
+
+
+START_TEST (test_create_destroy_dinamic_el)
+{
+	el_din = el_create ();
+	assert (el_din != NULL);
+
+	for(i=0; i < CANT_EDGES ; i++){
+		e = edge_create(i , i , i+1);
+		el_add_edge(el_din, e);
+	}
+
+	el_dinamic_destroy(el_din);
+	el_din= NULL;
+}
+END_TEST
+
+START_TEST ( test_check_size_dinamic_el )
+{
+	el_din = el_create ();
+	assert (el_din != NULL);
+
+	for(i=0; i < CANT_EDGES ; i++){
+		e = edge_create(i , i , i+1);
+		el_add_edge(el_din, e);
+	}
+
+	assert(el_get_size(el_din) == CANT_EDGES);
+	
+	el_dinamic_destroy(el_din);
+	el_din= NULL;
+	assert(el_get_size(el_din) == 0);
+	
+}
+END_TEST
+
+START_TEST ( test_start_avance_dinamic_el )
+{
+	el_din = el_create ();
+	assert (el_din != NULL);
+
+	for(i=0; i < CANT_EDGES ; i++){
+		e = edge_create(i , i , i+1);
+		el_add_edge(el_din, e);
+		e = el_get_actual(el_din);
+		assert (i== e->capacity);
+		assert (i== e->nodeOrig);
+		assert (i+1== e->nodeDest);
+	}
+
+	assert(el_get_size(el_din) == CANT_EDGES);
+	el_start(el_din);
+	do{
+		assert (i<= CANT_EDGES);
+		e = el_get_actual(el_din);
+		printf ("capacity = %u\n", e->capacity);
+		printf ("nodeOrig = %u\n", e->nodeOrig);
+		printf ("nodeDest = %u\n", e->nodeDest);
+	}while(el_avance(el_din)== 0);
+	
+	el_dinamic_destroy(el_din);
+	el_din= NULL;
+	assert(el_get_size(el_din) == 0);
+}
+END_TEST
+
+
+START_TEST ( test_start_avance_del_dinamic_el )
+{
+	el_din = el_create ();
+	assert (el_din != NULL);
+
+	for(i=0; i < CANT_EDGES ; i++){
+		e = edge_create(i , i , i+1);
+		el_add_edge(el_din, e);
+		e = el_get_actual(el_din);
+		assert (i== e->capacity);
+		assert (i== e->nodeOrig);
+		assert (i+1== e->nodeDest);
+	}
+
+	assert(el_get_size(el_din) == CANT_EDGES);
+	do{
+		assert (i<= CANT_EDGES);
+		e = el_get_actual(el_din);
+		printf ("capacity = %u\n", e->capacity);
+		printf ("nodeOrig = %u\n", e->nodeOrig);
+		printf ("nodeDest = %u\n", e->nodeDest);
+	}while(el_avance(el_din)== 0);
+
+	for(i=0; i < CANT_EDGES -10 ; i++){
+		el_del_edge(el_din);
+	}
+
+	assert(el_get_size(el_din) == 10);
+	el_dinamic_destroy(el_din);
+	el_din= NULL;
+	assert(el_get_size(el_din) == 0);
+	
+}
+END_TEST
+
+Suite *edge_list_suite (void)
+{
+	Suite *s = suite_create ("edge_list");
+	TCase *tc_commands = tcase_create ("Operationes");
+
+	/* Comandos */
+	tcase_add_test (tc_commands, test_create_destroy_dinamic_empty_el);
+	tcase_add_test (tc_commands, test_add_edge_dinamic_el);
+	tcase_add_test (tc_commands, test_create_destroy_dinamic_el);
+	tcase_add_test (tc_commands, test_check_size_dinamic_el);
+	tcase_add_test (tc_commands, test_start_avance_dinamic_el);
+	tcase_add_test (tc_commands, test_start_avance_del_dinamic_el);
+	suite_add_tcase (s,tc_commands);
+
+	return s;
+}
